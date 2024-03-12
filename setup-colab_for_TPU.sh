90444ffdc3baf69310247bcf6249ac7446e2e43d
nurikahraman <nurikahraman@gmail.com>
2024-03-12 15:46:55 +0300

    Update setup-colab_for_TPU.sh

--- setup-colab_for_TPU.sh
+++ setup-colab_for_TPU.sh
@@ -25,10 +25,10 @@ pip install -q 'git+https://github.com/facebookresearch/detectron2.git'# > /dev/
 
 #Datasets (This gets downloaded in "/content/foodChallenge/data")
 echo "🗄 Preparing the dataset for training..."
-unzip -q "/content/drive/MyDrive/Colab Notebooks/content/FoodRecognitionChallenge/val.zip" -d "/content/drive/MyDrive/Colab Notebooks/content/FoodRecognitionChallenge/dataset/"
+unzip -q "/content/drive/MyDrive/Colab Notebooks/content/FoodRecognitionChallenge/val.zip" -d "/content/dataset/"
 
 
 echo "🗄 Preparing the validation dataset..."
-unzip -q "/content/drive/MyDrive/Colab Notebooks/content/FoodRecognitionChallenge/train.zip" -d "/content/drive/MyDrive/Colab Notebooks/content/FoodRecognitionChallenge/dataset/"
+unzip -q "/content/drive/MyDrive/Colab Notebooks/content/FoodRecognitionChallenge/train.zip" -d "/content/dataset/"
 
 echo "All set! 🎉🍻"
